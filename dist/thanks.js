e810140be58fac4f9207c97e8765fdac32f78167
okondev2021 <sojapril16@gmail.com>
2024-02-08 09:41:32 +0100

    thank you page added

---
+++ dist/thanks.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const closeBtn = document.querySelector('.closeBtn');
+
+    closeBtn.addEventListener('click', () => {
+        window.location='index.html';
+    })
+})
